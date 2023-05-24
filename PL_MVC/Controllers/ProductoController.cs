7970ef2ab8ea26bdea3662dc502b7f47424c1bb8
BrianAguirreGuerra <briaguigue99@gmail.com>
2023-05-24 09:55:22 -0600

    validar

--- PL_MVC/Controllers/ProductoController.cs
+++ PL_MVC/Controllers/ProductoController.cs
@@ -66,7 +66,7 @@ namespace PL_MVC.Controllers
 
         }
 
-        [HttpPost] // Recibir los datos del formulario
+        [HttpPost] 
         public ActionResult Form(ML.Producto producto, HttpPostedFileBase ImgProducto)
         {
             producto.Imagen = ConvertToBytes(ImgProducto);
